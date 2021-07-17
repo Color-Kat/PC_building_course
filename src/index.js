b30ad8e909645d1cd7a6f1d01ba81cca1af7f67d
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-07-17 22:06:34 +0300

    fullscreen page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function ibg() {
     let ibg = document.querySelectorAll(".ibg");
     for (var i = 0; i < ibg.length; i++) {
         if (ibg[i].querySelector('img')) {
+            console.log(1243);
             ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
         }
     }
