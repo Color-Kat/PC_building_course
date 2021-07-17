8c707b003133181e11756d1ec2b5f749914d0b86
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-07-17 16:15:09 +0300

    header and introducing

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './scss/index.scss';
+
+// ibg - change html img for css background-img
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
